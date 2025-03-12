a059ab29d09a40f298d072c84043aac81f8c0d20
Daria Kokoreva <kokorewa.dashenka@yandex.ru>
2025-03-12 16:55:44 +0300

    Add createSheet function

--- new.js
+++ new.js
@@ -4,3 +4,12 @@ const ctx = canvas.getContext("2d");
 const SHEET_WIDTH = 862;
 const SHEET_HEIGHT = 604;
 let sheet;
+
+function createSheet() {
+    return {
+        width: SHEET_WIDTH,
+        height: SHEET_HEIGHT,
+        freeSpaces: new Set([{ x: 0, y: 0, width: SHEET_WIDTH, height: SHEET_HEIGHT }]),
+        rectangles: []
+    };
+}
